2d6d4caad18abbeb068453ff58b554eef528f7b3
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2024-09-09 19:28:39 +0530

    base code created

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+// database connection:
+
+
+// middlewares:
+
+
+// routes:
+
+
+app.listen(PORT, ()=> console.log(`Server is running succesfully at http://localhost:${PORT}`))
